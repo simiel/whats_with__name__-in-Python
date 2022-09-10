8cb817fa9a369aec62f8e4fadcb8fdae97033902
simiel <samensah036@st.ug.edu.gh>
2022-09-10 10:38:51 +0000

    O yeah

--- simiel.py
+++ simiel.py
@@ -2,8 +2,12 @@ print(__name__)
 # the above game me "__main__" as what is stored in __name__
 
 print(type(__name__))
-print(help(__name__))
-print(dir(__name__))
+# print(help(__name__))
+# print(dir(__name__))
 # It is a string object
 
 from simiel2 import simiel
+simiel()
+print(__name__)
+__name__ = '`SIMIEL`'
+print(__name__)
